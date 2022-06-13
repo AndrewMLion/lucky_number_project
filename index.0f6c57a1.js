186cf86c12400ae9e40f7e34ae35efd925495b37
AndrewMLion <34039980+AndrewMLion@users.noreply.github.com>
2022-06-13 17:14:27 +0000

    Deploying to gh-pages from @ AndrewMLion/lucky_number_project@d729edbc6bdd875d4aae3282ca2db95f12aaba74 🚀

---
+++ index.0f6c57a1.js
@@ -0,0 +1,2 @@
+var scrollUp=document.querySelector(".scroll-top");scrollUp.addEventListener("click",(function(o){window.scrollTo({top:0,behavior:"smooth"})})),window.addEventListener("scroll",(function(o){window.pageYOffset<100?scrollUp.classList.add("is-hidden"):scrollUp.classList.remove("is-hidden")}));
+//# sourceMappingURL=index.0f6c57a1.js.map
