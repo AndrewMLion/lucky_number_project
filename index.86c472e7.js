fab46e5c07c59f3906217ce8a240620a5a4c0c03
AndrewMLion <34039980+AndrewMLion@users.noreply.github.com>
2022-06-15 07:15:57 +0000

    Deploying to gh-pages from @ AndrewMLion/lucky_number_project@43d4857fde6a2720bb2a97a61dfd1fea6043030d 🚀

--- index.86c472e7.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal_about-open]"),closeModalBtn:document.querySelector("[data-modal_about-close]"),modal:document.querySelector("[data-modal_about]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.86c472e7.js.map
